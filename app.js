b6846129e2065913eefd8111cd43cac007813652
sergeev <eeasss@gmail.com>
2015-05-19 20:28:34 +0300

    Added two endpoints /tickers and /ticker?ticker=

--- app.js
+++ app.js
@@ -2,61 +2,72 @@ var express = require('express');
 var fs = require('fs');
 var https = require('https');
 var querystring = require('querystring');
-require('dns-notfound-what');
 var app = express();
 
 var host = 'https://query.yahooapis.com/v1/public/yql';
 var offset = 8 + 8 + 8  ; // 8 saturdays, 8 sundays, assume 8 public holidays
 var params = {
-    q: 'select * from yahoo.finance.historicaldata where symbol = "#ticker#" and startDate = "#start#" and endDate = "#end#"',
+    q: 'select * from yahoo.finance.quote where symbol in ("#ticker#")',
     format: 'json',
     env: 'store://datatables.org/alltableswithkeys',
 };
 
-app.get('/', function (req, res) {
-  res.send('Hello World!!!');
+app.get('/api/tickers', function (req, res) {
+   var promise = new Promise(function(resolve, reject) {
+         readStorage(function (err, data) {
+            if (err) {
+                throw err;
+            }
+
+            resolve(JSON.parse(data));
+         });   
+    });
+    
+    promise.then(function(val) { res.send(val); });
+    
 });
 
-app.get('/api/tickers', function (req, res) {
-    var obj;
-    fs.readFile('assets.json', 'utf8', function (err, data) {
-        if (err) {
-            throw err;
-        }
+app.get('/api/ticker', function(req, res) {
+    var assetName = req.query['ticker'];
+    var options = queryOptions(assetName);
+    params.q = params.q.replace('#ticker#', options.ticker);
+    var params_data = '?' + querystring.stringify(params);
 
-        var options = queryOptions('VYM');
-        params.q = params.q.replace("#ticker#", options.ticker).replace("#start#", options.start).replace("#end#", options.end);
-        var params_data = '?' + querystring.stringify(params);
+    var promise = new Promise(function(resolve, reject) {
         https.get(host + params_data, function(resp) { 
             var body = "";
- 
+
             resp.on('data', function(d) {
                 body += d;
                 console.log('data received');
             });
             
             resp.on('end', function(d) {
-               res.send(body);
+                var parsedBody = JSON.parse(body);
+                var price = parsedBody.query.results.quote.LastTradePriceOnly;
+                resolve(price);
             });
 
             resp.on('error', function(e) {
                 console.log('error');
-                res.send(e);
+                reject(e); 
             })
         });
     });
-});
 
-app.get('/api/test', function (req, res) {
-    res.send({hello: 'genady'});
+    promise.then(function(val) { res.send('{ price: ' + val + '}'); });
 });
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
-
+ 
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+function readStorage(callback) {
+    fs.readFile('assets.json', 'utf8', callback);
+}
 
 function date(offset) {
     var d = new Date();
