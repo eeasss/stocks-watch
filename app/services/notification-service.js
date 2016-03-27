0312748045aa3ae92fcbc95989b6a678c68eeeb1
sergeev <eeasss@gmail.com>
2016-03-27 23:46:48 +0300

    feat: added notification service

--- app/services/notification-service.js
+++ app/services/notification-service.js
@@ -1,4 +1,20 @@
- angular.module('app')
+angular.module('app')
     .factory('notification', [function() {
-    	
+        
+        var Notification = function() {
+            var observable = window.jQuery;
+        }
+        
+        Notification.prototype = {
+            bind: function(event, callback) {
+                jQuery(this).on(event, callback);
+            },
+            trigger: function(eventName, args) {
+                jQuery(this).trigger(eventName, args);
+            }
+        }
+        
+        return new Notification();
+        
     }]);
+
