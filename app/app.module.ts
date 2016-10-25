f5e3d16e2b8f91e5c85716b9a4ff0f2c749d4be7
sergeev <eeasss@gmail.com>
2016-10-25 23:22:12 +0300

    refactor: removed models module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,29 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { ModelsModule } from './models/models.module.ts';
 
 import { AppComponent } from './app.component';
 import { TickerViewerComponent } from  './ticker-viewer.component';
 
-import { NotificationService } from './notification.service.ts';
-import { QuoteService } from './quote.service.ts';
-import { ResultsService } from './results.service.ts';
-import { TickerService } from './ticker.service.ts';
-import { CurrencyService } from './currency.service.ts';
+import { NotificationService } from './notification.service';
+import { QuoteService } from './quote.service';
+import { ResultsService } from './results.service';
+import { TickerService } from './ticker.service';
+import { CurrencyService } from './currency.service';
 
 @NgModule({
     imports: [
         BrowserModule,
-        HttpModule,
-        ModelsModule
+        HttpModule
     ],
     declarations: [
         AppComponent,
         TickerViewerComponent
     ],
     exports: [
-        ModelsModule
     ],
     providers: [
         NotificationService,
